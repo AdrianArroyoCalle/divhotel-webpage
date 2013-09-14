f2571db4fc2184d126a0322023b3ff72f9e0650d
AdrianArroyoCalle <adrian.arroyocalle@gmail.com>
2013-09-14 12:41:44 +0200

    CSS Fixing

--- js/trial.js
+++ js/trial.js
@@ -2,5 +2,7 @@ function trial(json)
 {
 	//This function makes avalible the download button for downloading the trial version of DivHotel
 	var trial=document.getElementById("trial");
-	trial.textContent="Currently the trial is not avalible. Check later";
+	var text=document.createElement("p");
+	text.textContent="Currently the trial is not avalible. Check later";
+	trial.appendChild(text);
 }
