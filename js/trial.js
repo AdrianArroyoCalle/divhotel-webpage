9be2f1641fba7270bf370b2401f33b063d21fffb
AdrianArroyoCalle <adrian.arroyocalle@gmail.com>
2013-09-14 12:15:18 +0200

    Trial action

---
+++ js/trial.js
@@ -0,0 +1,6 @@
+function trial(json)
+{
+	//This function makes avalible the download button for downloading the trial version of DivHotel
+	var trial=document.getElementById("trial");
+	trial.textContent="Currently the trial is not avalible. Check later";
+}
